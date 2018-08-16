b4e77e2ed8109c5b9b89a084e2f81e474a24fad2
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-08-16 00:26:12 -0700

    Add tests for alphabetize

--- alphabetize.js
+++ alphabetize.js
@@ -7,3 +7,26 @@ var alphabetize = function(strArr) {
     return a.localeCompare(b);
   });
 };
+
+// TESTS:
+
+var assertEqual = function(actual, expected, testName) {
+  var actual = JSON.stringify(actual);
+  var expected = JSON.stringify(expected);
+
+  if (actual === expected) {
+    console.log('PASSED [' + testName + ']');
+  } else {
+      console.log('FAILED [' + testName + '] expected: ' + expected + '; but got: ' + actual);
+  }
+}
+
+var actual = alphabetize(['dog', 'cat', 'fish', 'bird']);
+var expected = (['bird', 'cat', 'dog', 'fish']);
+
+assertEqual(actual, expected, 'Alphabetize');
+
+var actual = alphabetize(['carrot', 'apple', 'banana']);
+var expected = (['apple', 'banana', 'carrot']);
+
+assertEqual(actual, expected, 'Alphabetize');
