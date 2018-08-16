ef62f515285daeb42d31ac19928868acdbb0086f
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-08-16 00:28:49 -0700

    Add tests for findLargestValue

--- findLargestValue.js
+++ findLargestValue.js
@@ -15,3 +15,26 @@ var findLargestValue = function(intArr) {
 // let findLargestValue = function(intArr) {
 //   return Math.max(...intArr);
 // };
+
+// TESTS:
+
+var assertEqual = function(actual, expected, testName) {
+  var actual = JSON.stringify(actual);
+  var expected = JSON.stringify(expected);
+
+  if (actual === expected) {
+    console.log('PASSED [' + testName + ']');
+  } else {
+      console.log('FAILED [' + testName + '] expected: ' + expected + '; but got: ' + actual);
+  }
+}
+
+var actual = findLargestValue([3, 5, 1, 44, 2, 6, 99]);
+var expected = 99;
+
+assertEqual(actual, expected, 'findLargestValue');
+
+var actual = findLargestValue([0, -10, 100, 2, 40, 66]);
+var expected = 100;
+
+assertEqual(actual, expected, 'findLargestValue');
