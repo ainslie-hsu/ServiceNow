986a585b0a953c72f5fa7720644d2284f4facedc
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-08-16 00:32:09 -0700

    Add addToHead method

--- linkedList.js
+++ linkedList.js
@@ -8,6 +8,21 @@ var LinkedList = function() {
   this.tail = null;
 };
 
+// Add to head:
+LinkedList.prototype.addToHead = function(str) {
+  var newHead = new Node(str);
+
+  if (!this.head && !this.tail) {
+    this.head = newHead;
+    this.tail = newHead;
+  } else {
+    newHead.next = this.head;
+    this.head = newHead;
+  }
+
+  return this;
+};
+
 // Node constructor:
 var Node = function(str) {
   this.value = str;
